b4c98f7f1ac9acc87346f22d5d32bde32b962d8f
drdokk <hi@drdokk.fr>
2022-01-14 12:04:25 +0100

    fetch details of single recipe based on slug (generating static paths)

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,45 @@
-export default function RecipeDetails() {
-  return (
-    <div>
-      Recipe Details
-    </div>
-  )
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+});
+
+// build time
+export const getStaticPaths = async () => {
+  const response = await client.getEntries({ content_type: "recipe" });
+
+  const paths = response.items.map((item) => {
+    return {
+      params: {
+        slug: item.fields.slug,
+      },
+    };
+  });
+
+  return {
+    //   paths: [{ params: { slug: 'a-slug'}}, ...]
+    paths,
+    // return 404 page if we try to reach a not existing path
+    fallback: false,
+  };
+};
+
+// fetch a single item based on the page we are on
+export const getStaticProps = async ({ params }) => {
+  // destructure "items" from reponse
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": params.slug,
+  });
+
+  // return the item to inject
+  return {
+    props: { recipe: items[0] },
+  };
+};
+
+export default function RecipeDetails({ recipe }) {
+  const { title } = recipe.fields;
+  return <div>Recipe Details: {title}</div>;
 }
